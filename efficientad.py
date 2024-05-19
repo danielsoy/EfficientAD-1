0d2ae8934d9715fb1f7227168afdda9558a45d36
danielsoy <48926149+danielsoy@users.noreply.github.com>
2024-05-19 11:06:02 -0300

    Update efficientad.py

--- efficientad.py
+++ efficientad.py
@@ -32,7 +32,8 @@ def get_argparse():
                              'download ImageNet and set to ImageNet path')
     parser.add_argument('-a', '--mvtec_ad_path',
                         
-                        default='/content/drive/MyDrive/mvtec_anomaly_detection/bottle',
+                        default='/content/drive/MyDrive/mvtec_anomaly_detection',
+                        
                        
                         help='Downloaded Mvtec AD dataset')
     parser.add_argument('-b', '--mvtec_loco_path',
